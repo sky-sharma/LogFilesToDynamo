09aa62d6e3e9a9f349b6284a309845cd97d01e9d
skysharma <sky.sharma@gmail.com>
2017-08-27 14:30:38 -0500

    Modified code to return column averages instead of sums.
    Added comments.

--- source/app.js
+++ source/app.js
@@ -5,13 +5,16 @@ const utils = require('./utils/utils.js');
 
 var appDir = path.dirname(require.main.filename);
 var logsDir = path.normalize(appDir + '/../LogFiles');
-var parsingPrefixes = ['Temperature: ', 'Pressure: '];
+var fields = ['Temperature', 'Pressure'];
 
 fs.readdir(logsDir, (err, logFiles) => {
   logFiles.forEach((logFile) => {
     fs.readFile((path.join(logsDir, logFile)), (err, logContents) => {
       if (err) throw err;
-      console.log(utils.parseStrForFloats(logContents, parsingPrefixes));
+      console.log(path.basename(logFile, '.txt'));
+      var floatsArray = utils.parseStrForFloats(logContents, fields);
+      console.log(floatsArray);
+      console.log(utils.calcColAvgs(floatsArray));
     });
   });
 });
--- source/utils/utils.js
+++ source/utils/utils.js
@@ -2,17 +2,43 @@ const Str = require('string');
 const scanf = require('scanf')
 const sscanf = require('scanf').sscanf;
 var floatsArray = [];
-var i = 0;
 var floatsRow = [];
 
-module.exports.parseStrForFloats = (dataStr, prefixes) => {
+module.exports.parseStrForFloats = (dataStr, fields) => {
+  // Receives a string (e.g. from a text file),extracts values of "fields"
+  // and returns those values as a 2D array of floats.
+
+  floatsArray = [];
   var dataRows = dataStr.toString().split('\n');
   dataRows.forEach((row) => {
     floatsRow = [];
-    prefixes.forEach((prefix) => {
-      floatsRow.push(sscanf(row, `${prefix}%f`));
+    fields.forEach((field) => {
+
+      // Parse out floating point values found after 'fieldname:\s'
+      // and create row of those values.
+      floatsRow.push(sscanf(row, `${field}: %f`));
     });
+
+    // Put row of floating point values into 2D array
     floatsArray.push(floatsRow);
   });
   return floatsArray;
 };
+
+module.exports.calcColAvgs = (floatsArray) => {
+  // Calculate avg. value of each column and return averages as a 1D array
+
+  var numElementsToAvg = floatsArray.length;
+  var numAvgs = floatsArray[0].length;
+  var colAvgs = [];
+  for (var j = 0; j < numAvgs; j++) {
+    var colSum = 0;
+    var colAvg = 0;
+    for (var i = 0; i < numElementsToAvg ; i++) {
+      colSum += floatsArray[i][j];
+    }
+    colAvg = colSum / numElementsToAvg;
+    colAvgs.push(colAvg);
+  }
+  return colAvgs;
+}
