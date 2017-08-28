4e64de71b141bf655487989b146435d0ad0a8ffd
skysharma <sky.sharma@gmail.com>
2017-08-27 20:09:48 -0500

    Commented out code that read from local file system and implemented code to read from S3 bucket.

--- source/app.js
+++ source/app.js
@@ -2,11 +2,44 @@ const fs = require('fs');
 const path = require('path');
 const Str = require('string');
 const utils = require('./utils/utils.js');
+const aws = require('aws-sdk');
 
 var appDir = path.dirname(require.main.filename);
 var logsDir = path.normalize(appDir + '/../LogFiles');
 var fields = ['Temperature', 'Pressure'];
+var s3 = new aws.S3();
 
+var params = {
+  Bucket: 'indie-log-files' /* required */};
+
+s3.listObjects(params, function(err, data) {
+  // List files in Bucket
+
+  if (err) console.log(err, err.stack); // an error occurred
+  else
+  {
+    var logFiles = data.Contents;
+    logFiles.forEach((logFile) => {
+      // Go through files in Bucket one by one
+
+      var logFileName = logFile.Key;
+      params.Key = logFileName;
+      // Get Log File Name
+
+      s3.getObject((params), (err, fileContents) => {
+        // Read contents of fileContents
+
+      if (err) throw err;
+      var logContents = fileContents.Body;
+      var floatsArray = utils.parseStrForFloats(logContents, fields);
+      console.log(path.basename(logFileName, '.txt'));
+      console.log(utils.calcColAvgs(floatsArray));
+    });
+  });
+  };
+});
+
+/*
 fs.readdir(logsDir, (err, logFiles) => {
   logFiles.forEach((logFile) => {
     fs.readFile((path.join(logsDir, logFile)), (err, logContents) => {
@@ -18,3 +51,4 @@ fs.readdir(logsDir, (err, logFiles) => {
     });
   });
 });
+*/
